e00b276ca8f4934603754228f198afb78027031a
senliontec <senliontec@163.com>
2021-12-06 11:23:17 +0800

    21_src_编程式路由导航

--- 20_src_编程式路由导航/pages/Home/Message/index.jsx
+++ 20_src_编程式路由导航/pages/Home/Message/index.jsx
@@ -20,6 +20,17 @@ export default class Message extends Component {
         this.props.history.push(`/home/message/detail/${id}/${title}`);
     }
 
+    back = () => {
+        this.props.history.goBack();
+    }
+
+    forward = () => {
+        this.props.history.goForward();
+    }
+
+    go = () => {
+        this.props.history.go(2);
+    }
     render() {
         const { messageArr } = this.state;
         return (
@@ -38,11 +49,11 @@ export default class Message extends Component {
                     }
                 </ul>
                 <hr />
-                <ul>
-                    <Route path="/home/message/detail/:id/:title" component={Detaill} />
-                </ul>
+                <Route path="/home/message/detail/:id/:title" component={Detaill} />
+                &nbsp; <button onClick={this.back}>back</button>
+                &nbsp; <button onClick={this.forward}>forward</button>
+                &nbsp; <button onClick={this.go}>go</button>
             </div >
-
         )
     }
 }
